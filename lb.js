d70ef0e73ae73323f7997828f26f728f39596c16
Eli Bittleston <dbittles@andrew.cmu.edu>
2019-04-26 13:55:34 -0400

    initial

---
+++ lb.js
@@ -0,0 +1,23 @@
+var config = {
+    apiKey: "AIzaSyBeSICfxHQthPIl8RH7R3ZFUxUe1Q7rIJY",
+    authDomain: "sydka-leaderboard.firebaseapp.com",
+    databaseURL: "https://sydka-leaderboard.firebaseio.com",
+    projectId: "sydka-leaderboard",
+    storageBucket: "sydka-leaderboard.appspot.com",
+    messagingSenderId: "61957478509"
+  };
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+function writeUserData(username, points) {
+  firebase.database().ref('users/' + userId).set({
+    points: points
+  });
+}
+
+function readUserPoints(username) {
+  console.log( firebase.database().ref('/users/' + username).once('value') );
+
+}
